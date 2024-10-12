df6d5968870598626c5252c94c7958d140cad651
TSShrol <shrolts@gmail.com>
2024-10-12 10:59:08 +0300

    11_files

--- 11_files/working_files.py
+++ 11_files/working_files.py
@@ -1,62 +1,100 @@
 import shelve
 import os
+# ==========record (write) in file
+# filewr=None
+# try:
+#     # dir_path="E:\\Tetiana\\CT_I_Python_2_2024\\11_files"
+#     dir_path="11_files"
+#     file_path=os.path.join(dir_path,"record.txt")
+#     print(file_path)
 
-# file_wr=""
-# E:\Tetiana\CT_I_Python_2_2024\11_files\working_files.py
-# CT_I_Python_2_2024\11_files => relative path
-# E:\Tetiana\CT_I_Python_2_2024\11_files\record.txt  => absolute path
-file_wr=None
+#     filewr=open(file_path,"wt", encoding="utf-8")
+#     text="""
+#     Mode files   ХY :
+#     X:
+#     r - читання
+#     w - запис в існуючий файл, або створення нового
+#     x - запис у файл, якщо файл не існує
+#     a - додавання в кінець файлу
 
-try:
-    # file_wr=open('CT_I_Python_2_2024\\11_files\\record.txt','at')
-    file_wr=open('Intro_Python/lec_file/record.txt','xt')
-    print(f'type=> {type(file_wr)}')
-    text="""Write to an Existing File
-    To write to an existing file, you must add a parameter to the open() function:
-    "a" - Append - will append to the end of the file
-    "w" - Write - will overwrite any existing content"""
-    print(text, file=file_wr)
-    file_wr.write(text+"\n")
-except FileExistsError:
-    print("record.txt already exists!")
-else:
-    file_wr.close()
+#     Y:
+#     t - файл текстовий (за замовчуванням)
+#     b - файл бінарний
+#     """
+#     # raise Exception("Checking Exception")
+#     res=filewr.write(text)
+# except FileNotFoundError as ex:
+#     print("Error=>", ex)
+# except Exception as ex:
+#    print("We are closed file")
+#    filewr.close()
+# else:
+#     print("Not exception")
+#     print("Count record bytes=>", res)
+#     filewr.close()
+# finally:
+#     print("close file")
 
-# read, readline(), readlines()
-file_r=open("Intro_Python/lec_file/record.txt","rt")
-text=file_r.read()
-# text=file_r.readlines()
+
+# # ======================= append and write from file 
+# file_append=""
+# # E:\Tetiana\CT_I_Python_2_2024\11_files\working_files.py
+# # 11_files => relative path відносний
+# # E:\Tetiana\CT_I_Python_2_2024\11_files\record.txt  => absolute path
+# try:
+#     # file_append=open('11_files/record.txt','at')
+#     file_append=open('11_files/record.txt','xt')
+#     print(f'type=> {type(file_append)}')
+#     text="""Write to an Existing File
+#     To write to an existing file, you must add a parameter to the open() function:
+#     "a" - Append - will append to the end of the file
+#     "w" - Write -will overwrite any existing content"""
+#     print(text+"\n", file=file_append)
+#     file_append.write(text+"\n")
+# except FileExistsError:
+#     print("record.txt already exists!")
+# else:
+#     file_append.close()
+
+
+# # #====================== read, readline(), readlines()
+# file_r=open("11_files/record.txt","rt")
+# # text=file_r.read()
+# # text=file_r.readlines()
+# # text=file_r.readline()
 # text=""
 # while True:
 #     line=file_r.readline()
 #     if not line:
 #         break
 #     text+=line
-words=file_r.split()
-file_r.close()
-print(text)
-# print(len(text))
+# words=text.split()
+# print(words)
+# file_r.close()
+# print(text)
+# print(len(text)) 
 
 
 # #======================================
-# file_r=open("Intro_Python\\lec_file\\record.txt","rt")
-# count_symbol=50
-# text1=''
+file_r=open("11_files\\record.txt","rt")
+count_symbol=100
+text1=''
 
-# while True:
-#     fragment=file_r.read(count_symbol)
-#     if not fragment:
-#         break
-#     text1+=fragment
-#     print(len(fragment))
+while True:
+    fragment=file_r.read(count_symbol)
+    if not fragment:
+        break
+    text1+=fragment
+    print(len(fragment))
+    print(fragment)
 
-# print("row=",file_r.read())
+print("row=",file_r.read())
 # # print("row=",file_r.read())
-# file_r.close()
-# print(text1)
+file_r.close()
+print(text1)
 #
 # print("*"*30)
-# file_r=open("record.txt","rt")
+# file_r=open("11_files\\record.txt","rt")
 # text2=""
 # line=file_r.readline()
 # while line:
@@ -64,19 +102,20 @@ print(text)
 #     line = file_r.readline()
 # file_r.close()
 # print(text2)
-#
-#
-# text3=""
-# file_r=open("Intro_Python\\lec_file\\record.txt","rt")
-# for line in file_r:
-#     text3+=line
 
-# file_r.close()
-# print(text3)
+#
+print("*"*50)
+text3=""
+file_r=open("11_files\\record.txt","rt")
+for line in file_r:
+    text3+=line
+
+file_r.close()
+print(text3)
 #
 #
 # text4=""
-# file_r=open("Intro_Python\\lec_file\\record.txt","rt")
+# file_r=open("11_files\\record.txt","rt")
 # lines=file_r.readlines()
 # print(lines)
 # file_r.close()
@@ -89,125 +128,130 @@ print(text)
 #
 # # ===================binary files==========================
 #
-
-# bindata=bytes(range(0,256))
-# print(bindata)
-# file_b_w=open("Intro_Python\\lec_file\\binary.dat","wb")
-# file_b_w.write(bindata)
+# \x89 => 10001001
+bindata=bytes(range(0,256))
+print(bindata)
+bindata_alpha=bytes(range(65,126))
+print(bindata_alpha)
+file_b_w=open("11_files\\binary.dat","wb")
+file_b_w.write(bindata_alpha)
 # text="""Write to an Existing File
 #     To write to an existing file, you must add a parameter to the open() function:
 #     "a" - Append - will append to the end of the file
-#     "w" - Write - will overwrite any existing content"""
-# # file_b_w.write(bytes(text,encoding="UTF-8"))
-# file_b_w.close()
-#
-#
-# file_b_w=open("Intro_Python\\lec_file\\binary.dat","wb")
+#     "w" - Write - will overwrite any existing content
+#     проба"""
+# file_b_w.write(bytes(text,encoding="UTF-8"))
+file_b_w.close()
+
+#=====record by fragmnet=======
+# file_b_w=open("11_files\\binary.dat","wb")
 # len_data=len(bindata)
 # setByte=0
 # step=50
 # while setByte<len_data:
-#     rez=file_b_w.write(bindata[setByte:setByte+step])
+#     rez=file_b_w.write(bindata[setByte:setByte+step]) #0:49 50:99... 
 #     print(rez)
 #     setByte+=step
 # file_b_w.close()
 #
-# file_b_r=open("Intro_Python\\lec_file\\binary.dat","rb")
-# print(file_b_r.read())
-# file_b_r.close()
+print("==read=="*5)
+file_b_r=open("11_files\\binary.dat","rb")
+print(file_b_r.read())
+file_b_r.close()
 #
 
 # print("====using tell()  and seek()")
 # # # tell seek
-# file_b_r=open("Intro_Python\\lec_file\\binary.dat","rb")
-# # print(file_b_r.tell())
-# # print(file_b_r.seek(90))
-# # print(file_b_r.tell())
-
-# # print(chr(97))
-# print(file_b_r.seek(97,0))
-# # print(file_b_r.seek(-148,2))
+# file_b_r=open("11_files\\binary.dat","rb")
+# print(file_b_r.tell()) #
+# print(file_b_r.seek(6)) # step moving => 90
 # print(file_b_r.tell())
-# # print(file_b_r.seek(4,1))
+
+# ## print(chr(97)) # when using bytes(range(0,266))
+# # print(file_b_r.seek(6,0))
 # # print(file_b_r.tell())
-# # print(chr(165))
-# bdata=file_b_r.read(26)
+# print(file_b_r.seek(5,1))
+# # print(file_b_r.tell())
+# # print(file_b_r.seek(4,2))
+# # print(file_b_r.tell())
+# # # print(chr(165))
+# bdata=file_b_r.read()
 # print(bdata)
 # file_b_r.close()
 #
-# with open("Intro_Python\\lec_file\\binary.dat","rb") as file_b_r:
-#     print(file_b_r.seek(97,0))
-#     # # print(file_b_r.seek(-148,2))
-#     # print(file_b_r.tell())
-#     # # print(file_b_r.seek(4,1))
-#     # # print(file_b_r.tell())
-#     # # print(chr(165))
-#     bdata=file_b_r.read(26)
-#     print(bdata)
+with open("11_files\\binary.dat","rb") as file_b_r:
+    # print(file_b_r.seek(32,0))
+    print(file_b_r.seek(-29,2))
+    print(file_b_r.tell())
+    # # print(file_b_r.seek(4,1))
+    # # print(file_b_r.tell())
+    bdata=file_b_r.read(26)
+    print(bdata)
 #
-# with open("binary.dat","rb") as fread:
-#     text=fread.read()
-#     print(text)
+with open("11_files\\binary.dat","rb") as fread:
+    bytes_res=fread.read()
+    convert_str=bytes_res.decode("UTF-8")
+    print(convert_str)
 
-# """
-#    ['Python', 'Guido van Rossum'],
-#     ['Scala', 'Martin Odersky'],
-#     ['PHP', 'Rasmus Lerdorf'],
-#     ['Ruby', 'Yukihiro Matsumoto'],
-#     ['C', 'Dennis Ritchie'],
-# """
+"""
+   ['Python', 'Guido van Rossum'],
+    ['Scala', 'Martin Odersky'],
+    ['PHP', 'Rasmus Lerdorf'],
+    ['Ruby', 'Yukihiro Matsumoto'],
+    ['C', 'Dennis Ritchie'],
+"""
 
-# filename = "shelve_ex"
+filename = "11_files\\shelve_ex"
+# working with files for dictonary
+with shelve.open(filename, 'c') as shelve_wr:
+    shelve_wr['Python'] = 'Guido van Rossum'
+    shelve_wr['Scala'] = 'Martin Odersky'
+    shelve_wr['C'] = 'Dennis Ritchie'
 
-# with shelve.open(filename, 'c') as shelve_wr:
-#     shelve_wr['Python'] = 'Guido van Rossum'
-#     shelve_wr['Scala'] = 'Martin Odersky'
-#     shelve_wr['C'] = 'Dennis Ritchie'
+with shelve.open(filename, 'r') as shelve_r:
+    key="Scala"
+    if key in shelve_r:
+        print(shelve_r[key])
+    print(shelve_r['Python'])
+    print(shelve_r['Scala'])
 
-# with shelve.open(filename, 'r') as shelve_r:
-#     key="Scala"
-#     if key in shelve_r:
-#         print(shelve_r[key])
-    # print(shelve_r['Python'])
-    # print(shelve_r['Scala'])
+with shelve.open(filename, 'r') as shelve_r:
+    value=shelve_r.get("C","None")
+    print(value)
 
-# with shelve.open(filename, 'r') as shelve_r:
-#     value=shelve_r.get("C","None")
-#     print(value)
-
-# with shelve.open(filename, 'r') as shelve_r:
-#     for key in shelve_r:
-#         print(key," ",shelve_r[key])
-
-
-# with shelve.open(filename, 'r') as shelve_r:
-#     for avtor in shelve_r.values():
-#         print(avtor)
-
-# with shelve.open(filename, 'c') as shelve_wandr:
-#     shelve_wandr["C"]="Ritchie"
+with shelve.open(filename, 'r') as shelve_r:
+    for key in shelve_r:
+        print(key," ",shelve_r[key])
 
 
-# with shelve.open(filename, 'r') as shelve_r:
-#     for avtor in shelve_r.values():
-#         print(avtor)
+with shelve.open(filename, 'r') as shelve_r:
+    for avtor in shelve_r.values():
+        print(avtor)
+
+with shelve.open(filename, 'c') as shelve_wandr:
+    shelve_wandr["C"]="Ritchie"
 
 
-# # os.mkdir("example1")
-# # os.mkdir("D://Example1")
-# # os.mkdir("D://Example1/exam")
-# if os.path.exists("D://Example1/exam/f2.txt"):
-#     os.rename("D://Example1/exam/f2.txt","D://Example1/exam/f1.txt")
-# else:
-#     print("ERROR")
+with shelve.open(filename, 'r') as shelve_r:
+    for avtor in shelve_r.values():
+        print(avtor)
 
 
-# # filename="D://Example1/exam/f1.txt"
-# with open("listSt.txt",'rt') as fileR:
-#     # rez=fileR.read()
-#     rez=fileR.readlines()
+# os.mkdir("example1")
+# os.mkdir("E://Example1")
+# os.mkdir("E://Example1/exam")
+if os.path.exists("E://Example1/exam/f2.txt"):
+    os.rename("E://Example1/exam/f2.txt","E://Example1/exam/f1.txt")
+else:
+    print("ERROR")
 
-# print(rez)
+
+filename="E:/Example1/exam/f1.txt"
+with open(filename,'rt') as fileR:
+    rez=fileR.read()
+    # rez=fileR.readlines()
+
+print(rez)
 # with open("listSt.txt",'wt') as fileW:
 #     for row in rez:
 #         print(row.rstrip("\n"))
